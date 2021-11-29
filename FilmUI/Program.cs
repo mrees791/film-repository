a8e2e36e9ef77089ce4d33ab7ac60eedecc7a12b
Michael Rees <mrees791@gmail.com>
2021-11-29 15:14:01 -0500

    Created FilmUI module.

---
+++ FilmUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FilmUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Program().Start();
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Film Repository App");
+        }
+    }
+}
