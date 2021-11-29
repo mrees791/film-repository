7f33756e93e12ef2bd091186f8ff6d971a955930
Michael Rees <mrees791@gmail.com>
2021-11-29 15:12:38 -0500

    Created FilmDatabase class.

---
+++ FilmLibrary/FilmDatabase.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using FilmLibrary.Tables;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmLibrary
+{
+    public class FilmDatabase
+    {
+        private readonly string ConnectionString;
+
+        public FilmDatabase(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public IEnumerable<Country> GetCountries()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                return connection.Query<Country>("SELECT * FROM Country;");
+            }
+        }
+    }
+}
